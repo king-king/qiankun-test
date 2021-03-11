3bb1c9774f5225dbd409f4f0f196b19a93ee06cd
wangqun6 <wangqun28>
2021-03-11 21:53:59 +0800

    change

--- app/react-app/src/App.js
+++ app/react-app/src/App.js
@@ -1,17 +1,59 @@
+import React from "react";
+import {
+    BrowserRouter ,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+export default function App() {
     return (
-        <div className="App">
-            <header className="App-header">
+        <BrowserRouter basename="/views/app-react">
+            <div className='App-header'>
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    React应用已启动
-                </p>
-            </header>
-        </div>
+                <nav>
+                    <ul className='link'>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/users">Users</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/users">
+                        <Users />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 }
 
-export default App;
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Users() {
+    return <h2>Users</h2>;
+}
