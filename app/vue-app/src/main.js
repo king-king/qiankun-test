12885222454f53cddf8bf0b552e5eef43c1ec1d1
wangqun6 <wangqun28>
2021-03-11 18:22:27 +0800

    change

--- app/vue-app/src/main.js
+++ app/vue-app/src/main.js
@@ -1,8 +1,33 @@
+import './public-path';
+
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+function render(props = {}) {
+    const { container } = props;
+
+    new Vue({
+        render: h => h(App),
+    }).$mount(container ? container.querySelector('#app') : '#app')
+}
+
+// 独立运行时
+if (!window.__POWERED_BY_QIANKUN__) {
+    render();
+}
+
+export async function bootstrap() {
+    console.log('[vue] vue app bootstraped');
+}
+export async function mount(props) {
+    console.log('[vue] props from main framework', props);
+    render(props);
+}
+export async function unmount() {
+    instance.$destroy();
+    instance.$el.innerHTML = '';
+    instance = null;
+    router = null;
+}
