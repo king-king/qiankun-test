b121b4fa48e7b6184c32efe1afbf3b5975c9735f
wangqun6 <wangqun28>
2021-03-11 19:21:45 +0800

    change

--- app/react-app/src/App.js
+++ app/react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                    React应用已启动
                 </p>
             </header>
         </div>
